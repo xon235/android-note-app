44426efe91b12d41f07c33caf970d298e738a202
shy.yi <shy.yi@kakaocorp.com>
2022-01-10 16:28:18 +0900

    Add Missing RoomDatabase Inheritance to NoteDatabase.kt

--- app/src/main/java/com/survivalcoding/noteapp/data/data_source/NoteDatabase.kt
+++ app/src/main/java/com/survivalcoding/noteapp/data/data_source/NoteDatabase.kt
@@ -1,9 +1,10 @@
 package com.survivalcoding.noteapp.data.data_source
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.survivalcoding.noteapp.domain.model.Note
 
 @Database(entities = [Note::class], version = 1)
-abstract class NoteDatabase {
+abstract class NoteDatabase: RoomDatabase() {
     abstract fun noteDao(): NoteDao
 }
