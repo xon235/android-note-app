f407b2889027117292eb13a0097ea4958f087243
shy.yi <shy.yi@kakaocorp.com>
2022-01-11 00:27:31 +0900

    Create GetFlowOrderByUseCase.kt

---
+++ app/src/main/java/com/survivalcoding/noteapp/domain/usecase/GetFlowOrderByUseCase.kt
@@ -0,0 +1,16 @@
+package com.survivalcoding.noteapp.domain.usecase
+
+import com.survivalcoding.noteapp.domain.repository.SelectAllRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
+
+class GetFlowOrderByUseCase<T>(private val repository: SelectAllRepository<T>) {
+    operator fun invoke(
+        comparatorFlow: Flow<Comparator<T>>,
+        isAscFlow: Flow<Boolean>
+    ): Flow<List<T>> {
+        return combine(repository.selectAll(), comparatorFlow, isAscFlow) { ts, comparator, isAsc ->
+            ts.sortedWith(comparator).apply { if (!isAsc) reversed() }
+        }
+    }
+}
