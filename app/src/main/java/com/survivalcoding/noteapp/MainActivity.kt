175728f5bcde9fa67a1c300375a577cc23d25ae4
shy.yi <shy.yi@kakaocorp.com>
2022-01-10 15:31:30 +0900

    Apply ViewBinding to MainActivity.kt

--- app/src/main/java/com/survivalcoding/noteapp/MainActivity.kt
+++ app/src/main/java/com/survivalcoding/noteapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.survivalcoding.noteapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.survivalcoding.noteapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
