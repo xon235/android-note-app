a147557ac64ce33f298c0d2aa600517d1525120d
shy.yi <shy.yi@kakaocorp.com>
2022-01-10 16:49:03 +0900

    Add Initial Implementations of ViewModel and ViewModelFactory

--- app/src/main/java/com/survivalcoding/noteapp/presentation/add_edit_note/AddEditNoteViewModel.kt
+++ app/src/main/java/com/survivalcoding/noteapp/presentation/add_edit_note/AddEditNoteViewModel.kt
@@ -1,4 +1,21 @@
 package com.survivalcoding.noteapp.presentation.add_edit_note
 
-class AddEditNoteViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.survivalcoding.noteapp.domain.model.Note
+import com.survivalcoding.noteapp.domain.repository.NoteRepository
+import kotlinx.coroutines.launch
+
+class AddEditNoteViewModel(private val noteRepository: NoteRepository): ViewModel() {
+
+    private val _note = MutableLiveData<Note>()
+    val note: LiveData<Note> = _note
+
+    fun getNoteById(id: Int) {
+        viewModelScope.launch {
+            _note.value = noteRepository.getNoteById(id)
+        }
+    }
 }
--- app/src/main/java/com/survivalcoding/noteapp/presentation/notes/NotesViewModel.kt
+++ app/src/main/java/com/survivalcoding/noteapp/presentation/notes/NotesViewModel.kt
@@ -1,4 +1,19 @@
 package com.survivalcoding.noteapp.presentation.notes
 
-class NotesViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.survivalcoding.noteapp.domain.model.Note
+import com.survivalcoding.noteapp.domain.repository.NoteRepository
+import kotlinx.coroutines.launch
+
+class NotesViewModel(private val noteRepository: NoteRepository): ViewModel() {
+
+    val notes = noteRepository.getNotes().asLiveData()
+
+    fun deleteNote(note: Note) {
+        viewModelScope.launch {
+            noteRepository.deleteNote(note)
+        }
+    }
 }
