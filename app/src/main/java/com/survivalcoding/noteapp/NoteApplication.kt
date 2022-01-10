f4a473c0fe9e6238687ea9988d5e3c9694dff5c1
shy.yi <shy.yi@kakaocorp.com>
2022-01-10 16:47:50 +0900

    Add noteDatabase and noteRepository to NoteApplication.kt

--- app/src/main/java/com/survivalcoding/noteapp/NoteApplication.kt
+++ app/src/main/java/com/survivalcoding/noteapp/NoteApplication.kt
@@ -1,7 +1,17 @@
 package com.survivalcoding.noteapp
 
 import android.app.Application
+import androidx.room.Room
+import com.survivalcoding.noteapp.data.data_source.NoteDatabase
+import com.survivalcoding.noteapp.data.repository.NoteRoomRepository
 
 class NoteApplication : Application() {
 
+    private val noteDatabase by lazy {
+        Room.databaseBuilder(this, NoteDatabase::class.java, "note_db").build()
+    }
+
+    val noteRepository by lazy {
+        NoteRoomRepository(noteDatabase.noteDao())
+    }
 }
