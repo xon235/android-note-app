dc92cc20701240a40223df325bb3deb28d135ecd
shy.yi <shy.yi@kakaocorp.com>
2022-01-10 15:34:46 +0900

    Add Database Annotation to NoteDatabase.kt and Add NoteDao Declaration

--- app/src/main/java/com/survivalcoding/noteapp/data/data_source/NoteDatabase.kt
+++ app/src/main/java/com/survivalcoding/noteapp/data/data_source/NoteDatabase.kt
@@ -1,4 +1,9 @@
 package com.survivalcoding.noteapp.data.data_source
 
+import androidx.room.Database
+import com.survivalcoding.noteapp.domain.model.Note
+
+@Database(entities = [Note::class], version = 1)
 abstract class NoteDatabase {
+    abstract fun noteDao(): NoteDao
 }
