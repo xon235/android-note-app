65b5436600b657768e4070233c13255b02f60e23
shy.yi <shy.yi@kakaocorp.com>
2022-01-10 15:33:03 +0900

    Add Dao Annotation to NoteDao.kt and Query Declarations

--- app/src/main/java/com/survivalcoding/noteapp/data/data_source/NoteDao.kt
+++ app/src/main/java/com/survivalcoding/noteapp/data/data_source/NoteDao.kt
@@ -1,4 +1,21 @@
 package com.survivalcoding.noteapp.data.data_source
 
+import androidx.room.*
+import com.survivalcoding.noteapp.domain.model.Note
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface NoteDao {
+
+    @Query("SELECT * FROM Note")
+    fun selectAll(): Flow<List<Note>>
+
+    @Query("SELECT * FROM Note WHERE id == :id")
+    suspend fun selectById(id: Int): Note?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(vararg note: Note)
+
+    @Delete
+    suspend fun delete(vararg note: Note)
 }
