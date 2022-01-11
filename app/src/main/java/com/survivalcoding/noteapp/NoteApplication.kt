f686593fe1854901eccbe8534c869e625bf99af5
shy.yi <shy.yi@kakaocorp.com>
2022-01-11 20:44:04 +0900

    Add Test Rows to Database

--- app/src/main/java/com/survivalcoding/noteapp/NoteApplication.kt
+++ app/src/main/java/com/survivalcoding/noteapp/NoteApplication.kt
@@ -4,6 +4,9 @@ import android.app.Application
 import androidx.room.Room
 import com.survivalcoding.noteapp.data.datasource.NoteDatabase
 import com.survivalcoding.noteapp.data.repository.NoteRoomRepository
+import com.survivalcoding.noteapp.domain.model.Note
+import kotlinx.coroutines.runBlocking
+import kotlin.random.Random
 
 class NoteApplication : Application() {
 
@@ -12,6 +15,11 @@ class NoteApplication : Application() {
     }
 
     val noteRepository by lazy {
+
+        // Add Default Data...
+        for(i in 1..30) {
+            runBlocking { noteDatabase.noteDao().upsert(Note(i, "Title #${Random.nextInt(0, 1000)}", "Content #${Random.nextInt(0, 1000)}")) }
+        }
         NoteRoomRepository(noteDatabase.noteDao())
     }
 }
