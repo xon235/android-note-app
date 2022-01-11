9c7f15f409ab3bc9fc300b45d77a9b3a47b7b492
shy.yi <shy.yi@kakaocorp.com>
2022-01-11 18:34:00 +0900

    Replace Action Bar with Toolbar

--- app/src/main/java/com/survivalcoding/noteapp/presentation/MainActivity.kt
+++ app/src/main/java/com/survivalcoding/noteapp/presentation/MainActivity.kt
@@ -2,11 +2,10 @@ package com.survivalcoding.noteapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.survivalcoding.noteapp.R
 import com.survivalcoding.noteapp.databinding.ActivityMainBinding
@@ -19,14 +18,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostF) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostF) as NavHostFragment
         val navController = navHostFragment.navController
-        setupActionBarWithNavController(navController)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.navHostF)
         val appBarConfiguration = AppBarConfiguration(navController.graph)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.toolbar.visibility =
+                if (destination.id == R.id.noteFragment) View.GONE else View.VISIBLE
+        }
     }
 }
