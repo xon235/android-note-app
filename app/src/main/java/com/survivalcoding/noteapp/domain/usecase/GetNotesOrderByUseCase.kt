b01757168119f6a5f9a43ba6e456fc406d3e6497
shy.yi <shy.yi@kakaocorp.com>
2022-01-13 18:12:52 +0900

    Rewrite GetFlowOrderByUseCase.kt Without Generics and SelectAllRepository and Change Its Name to GetNotesOrderByUseCase.kt

---
+++ app/src/main/java/com/survivalcoding/noteapp/domain/usecase/GetNotesOrderByUseCase.kt
@@ -0,0 +1,24 @@
+package com.survivalcoding.noteapp.domain.usecase
+
+import com.survivalcoding.noteapp.domain.NoteOrderBy
+import com.survivalcoding.noteapp.domain.model.Note
+import com.survivalcoding.noteapp.domain.repository.NoteRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flatMapLatest
+
+class GetNotesOrderByUseCase(private val repository: NoteRepository) {
+    @ExperimentalCoroutinesApi
+    operator fun invoke(
+        noteOrderByFlow: Flow<NoteOrderBy>,
+        isAscFlow: Flow<Boolean>
+    ): Flow<List<Note>> {
+        return combine(noteOrderByFlow, isAscFlow) {noteOrderBy, isAsc ->
+            Pair(noteOrderBy, isAsc)
+        }.flatMapLatest { pair ->
+            val (noteOrderBy, isAsc) = pair
+            repository.selectAllOrderBy(noteOrderBy.columnName, isAsc)
+        }
+    }
+}
