a5add73b7816c2cb39dc605b6b34fa0cfbad15bc
shy.yi <shy.yi@kakaocorp.com>
2022-01-13 18:08:56 +0900

    Change NoteDao.kt to Abstract Class and Add fun selectAllOrderBy

--- app/src/main/java/com/survivalcoding/noteapp/data/datasource/NoteDao.kt
+++ app/src/main/java/com/survivalcoding/noteapp/data/datasource/NoteDao.kt
@@ -1,21 +1,32 @@
 package com.survivalcoding.noteapp.data.datasource
 
 import androidx.room.*
+import androidx.sqlite.db.SimpleSQLiteQuery
+import androidx.sqlite.db.SupportSQLiteQuery
 import com.survivalcoding.noteapp.domain.model.Note
 import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface NoteDao {
+abstract class NoteDao {
 
     @Query("SELECT * FROM Note")
-    fun selectAll(): Flow<List<Note>>
+    abstract fun selectAll(): Flow<List<Note>>
 
     @Query("SELECT * FROM Note WHERE id == :id")
-    suspend fun selectById(id: Int): Note?
+    abstract suspend fun selectById(id: Int): Note?
+
+
+    fun selectAllOrderBy(columnName: String, isAsc: Boolean): Flow<List<Note>> {
+        val query = "SELECT * FROM Note ORDER BY $columnName ${if(isAsc) "ASC" else "DESC"}"
+        return rawQueryNotes(SimpleSQLiteQuery(query))
+    }
+
+    @RawQuery(observedEntities = [Note::class])
+    protected abstract fun rawQueryNotes(query: SupportSQLiteQuery): Flow<List<Note>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun upsert(vararg note: Note)
+    abstract suspend fun upsert(vararg note: Note)
 
     @Delete
-    suspend fun delete(vararg note: Note)
+    abstract suspend fun delete(vararg note: Note)
 }
