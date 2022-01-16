4b7bac6d2a9809491df1d30d4e5a02c1ef9d0a10
shy.yi <shy.yi@kakaocorp.com>
2022-01-16 23:59:36 +0900

    Change The UI to Look More Like The Original Design

--- app/src/main/java/com/survivalcoding/noteapp/presentation/notes/adapter/NoteViewHolder.kt
+++ app/src/main/java/com/survivalcoding/noteapp/presentation/notes/adapter/NoteViewHolder.kt
@@ -1,6 +1,10 @@
 package com.survivalcoding.noteapp.presentation.notes.adapter
 
+import android.content.res.ColorStateList
+import android.graphics.PorterDuff
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.survivalcoding.noteapp.R
 import com.survivalcoding.noteapp.databinding.ItemNoteBinding
 import com.survivalcoding.noteapp.domain.model.Note
 
@@ -9,6 +13,9 @@ class NoteViewHolder(private val binding: ItemNoteBinding): RecyclerView.ViewHol
     fun bind(note: Note, onClick: (Note) -> Unit, onDelete: (Note) -> Unit) {
         binding.titleTv.text = note.title
         binding.contentTv.text = note.content
+        binding.root.backgroundTintMode = PorterDuff.Mode.MULTIPLY
+        binding.root.background.setTint(note.color)
+        binding.foldIv.setColorFilter(note.color, PorterDuff.Mode.MULTIPLY);
         binding.root.setOnClickListener { onClick(note) }
         binding.deleteIb.setOnClickListener { onDelete(note) }
     }
